5704eff5c6188a07ce216b02feaf22da9132bf84
Henry Ladstaetter <henry.ladstaetter08@gmail.com>
2023-11-17 09:07:58 +0100

    First Commit

---
+++ 013_Restaurant-Queue/Queue.App/Program.cs
@@ -0,0 +1,14 @@
+﻿using Queue.Logic;
+int choice;
+System.Console.WriteLine("What do you want to do ?");
+System.Console.WriteLine("1) Add a customer to the queue");
+System.Console.WriteLine("2) Seat the next customer");
+System.Console.WriteLine("3) Display the queue");
+System.Console.WriteLine("4) Exit the programm");
+
+do
+{System.Console.Write("Your choice : ");
+int.TryParse(Console.ReadLine()!, out choice);
+
+
+}while(choice != 4);
---
+++ 013_Restaurant-Queue/Queue.Logic/CostumerInQeue.cs
@@ -0,0 +1,9 @@
+﻿namespace Queue.Logic;
+
+class CustomerInQueue(string name, string phonenumber, CustomerInQueue? next)
+{
+    public string Name { get; set; } = name;
+    public string PhoneNumber { get; set; } = phonenumber;
+    public CustomerInQueue? Next { get; set; } = next;
+}
+
